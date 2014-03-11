140eb7bfcc286681952f80a4ca3e32d9021c0c12
Alberto Cardellino <alberto.cardellino@iit.it>
2014-03-11 11:13:03 +0100

    Added empty implementation of InteractionMode Interface in the motionControl devices.

--- src/libraries/icubmod/canBusMotionControl/CanBusMotionControl.h
+++ src/libraries/icubmod/canBusMotionControl/CanBusMotionControl.h
@@ -674,6 +674,8 @@ class yarp::dev::CanBusMotionControl:public DeviceDriver,
             public ImplementOpenLoopControl,
             public ImplementControlMode,
             public ImplementDebugInterface,
+            public IInteractionModeRaw,
+            public ImplementInteractionMode,
             public IFactoryInterface,
             public IClientLogger
 {
@@ -1006,6 +1008,14 @@ public:
     bool setPositionRaw(int j, double ref);
     bool setPositionsRaw(const int n_joint, const int *joints, double *refs);
     bool setPositionsRaw(const double *refs);
+
+    // InteractionMode interface
+    bool getInteractionModeRaw(int axis, yarp::dev::InteractionModeEnum* mode);
+    bool getInteractionModesRaw(int n_joints, int *joints, yarp::dev::InteractionModeEnum* modes);
+    bool getInteractionModesRaw(yarp::dev::InteractionModeEnum* modes);
+    bool setInteractionModeRaw(int axis, yarp::dev::InteractionModeEnum mode);
+    bool setInteractionModesRaw(int n_joints, int *joints, yarp::dev::InteractionModeEnum* modes);
+    bool setInteractionModesRaw(yarp::dev::InteractionModeEnum* modes);
 
 protected:
     bool setBCastMessages (int axis, unsigned int v);
