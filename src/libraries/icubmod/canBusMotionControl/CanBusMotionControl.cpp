892d59d54a90aab234af07bd4264d5c2432bde31
Marco Randazzo <marco.randazzo@iit.it>
2014-05-28 19:49:47 +0200

    fixed bug related to signed/unsigned control modes

--- src/libraries/icubmod/canBusMotionControl/CanBusMotionControl.cpp
+++ src/libraries/icubmod/canBusMotionControl/CanBusMotionControl.cpp
@@ -195,11 +195,11 @@ public:
     short _axisStatus;
     char  _canStatus;
     char  _boardStatus;
-    char  _controlmodeStatus;
+    unsigned char _controlmodeStatus;
     double _update_e;
 
     // msg 3b
-    char  _interactionmodeStatus;
+    unsigned char _interactionmodeStatus;
     double _update_e2;
 
     // msg 4
@@ -3534,7 +3534,7 @@ bool CanBusMotionControl::getControlModesRaw(int *v)
 
 //---------------------------------------------------------
 
-int CanBusMotionControl::from_interactionvocab_to_interactionint (int interactionvocab)
+unsigned char  CanBusMotionControl::from_interactionvocab_to_interactionint (int interactionvocab)
 {
     switch (interactionvocab)
     {
@@ -3551,7 +3551,7 @@ int CanBusMotionControl::from_interactionvocab_to_interactionint (int interactio
     }
 }
 
-int CanBusMotionControl::from_interactionint_to_interactionvocab (int interactionint)
+int CanBusMotionControl::from_interactionint_to_interactionvocab (unsigned char interactionint)
 {
     switch (interactionint)
     {
@@ -3569,7 +3569,7 @@ int CanBusMotionControl::from_interactionint_to_interactionvocab (int interactio
 }
 
 
-int CanBusMotionControl::from_modevocab_to_modeint (int modevocab)
+unsigned char CanBusMotionControl::from_modevocab_to_modeint (int modevocab)
 {
     switch (modevocab)
     {
@@ -3611,7 +3611,7 @@ int CanBusMotionControl::from_modevocab_to_modeint (int modevocab)
     }
 }
 
-int CanBusMotionControl::from_modeint_to_modevocab (int modeint)
+int CanBusMotionControl::from_modeint_to_modevocab (unsigned char modeint)
 {
     switch (modeint)
     {
